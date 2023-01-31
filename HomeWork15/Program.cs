d26d12b347b56178e7669536fe9ef2ef635388a3
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-31 22:51:20 +0300

    HomeWork15/

---
+++ HomeWork15/Program.cs
@@ -0,0 +1,25 @@
+﻿static void IfHolliday(int dayNumber)
+{
+    if(dayNumber >= 1 && dayNumber <= 5) 
+    {
+        System.Console.WriteLine("Будний день");
+    }
+    else if(dayNumber >= 6 && dayNumber <= 7)
+    {
+        System.Console.WriteLine("Выходной день");
+    }
+    else
+    {
+        System.Console.WriteLine("Нет такого дня недели!");
+    }
+}
+
+System.Console.WriteLine("Проверка на выходной день.");
+System.Console.Write("Введите номер дня недели: ");
+int number;
+
+while(!int.TryParse(Console.ReadLine(), out number))
+{
+    System.Console.Write("Неправильный ввод данных! Введите число: ");
+}
+IfHolliday(number);
