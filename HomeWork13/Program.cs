23a51ab72ddf4bad4b838b8c2611298405cdae43
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-31 22:51:03 +0300

    HomeWork13/

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,30 @@
+﻿static void FindThirdDigit(double number)
+{
+    int result;
+    number = Math.Abs(number);
+    if (number > 99)
+    {
+        while (number > 999)
+        {
+            number /= 10;
+        }
+        result = (int)number % 10;
+        System.Console.WriteLine($"Третья цифра в числе: {result}");
+    }
+    else
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+
+System.Console.WriteLine("Необходимо вывести третью цифру заданного числа или сообщить, что третьей цифры нет.");
+double number;
+System.Console.Write("Введите любое число: ");
+while(!double.TryParse(Console.ReadLine(), out number))
+{
+    System.Console.Write("Неправильный ввод данных! Введите число: ");
+}
+
+FindThirdDigit(number);
+
